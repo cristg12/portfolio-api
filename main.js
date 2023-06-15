1533c7e301a54358edc0b86e1994f8d5c2c7a838
Cristian Trenado <cristiantrenado41@gmail.com>
2023-06-15 18:22:32 +0200

    actual work

--- main.js
+++ main.js
@@ -1,74 +1,71 @@
-const express = require('express');
-const app= express();
-
-// let projects = [];
+const express = require("express");
+const app = express();
 
 let projects = [
-    { id: 1, name: "Mi proyecto 1", img: "/assets/images/shop.svg" },
-    { id: 2, name: "Mi proyecto 2", img: "/assets/images/shop.svg" },
-    { id: 3, name: "Mi proyecto 3", img: "/assets/images/shop.svg" },
-    { id: 4, name: "Mi proyecto 4", img: "/assets/images/shop.svg" },
-    { id: 5, name: "Mi proyecto 5", img: "/assets/images/shop.svg" },
-    ];
+  { id: 2, name: "Mi proyecto potato 2", img: "/assets/images/shop.svg" }, //0
+  { id: 1, name: "Mi proyecto carrot 1", img: "/assets/images/shop.svg" }, //1
+  { id: 3, name: "Mi proyecto potato 3", img: "/assets/images/shop.svg" }, //2
+  { id: 4, name: "Mi proyecto carrot 4", img: "/assets/images/shop.svg" }, //3
+];
 
-app.use(express.json())
+app.use(express.json());
 
-
-app.get('/projects', (req, res)=>{
-    res.send(projects);
+app.get("/projects", (req, res) => {
+  res.send(projects);
 });
 
-app.post("/projects", (req, res)=>{
-    const project = req.body;
-    projects.push(project);
-    res.send("ok!");
-})
+app.post("/projects", (req, res) => {
+  const project = req.body;
+  projects.push(project);
+  res.send("ok!");
+});
 
-app.get("/projects/:id", (req, res)=>{
-    console.log("Params:", req.params); //{id:"123"}
-    console.log("Query:", req.query);//{param 1:"123"}
-    const id = req.params.id;
-    console.log("ID:", id);
-    const project = projects.find((p)=> p.id == id);
-    if(!project){
-        res.status(404);
-    }
-    res.send (project);
-})
+app.get("/projects/:id", (req, res) => {
+  const id = req.params.id;
+  const project = projects.find((p) => p.id == id);
 
-// eliminar elemento
-app.delete("/projects/:id", (req, res)=>{
-    // obtener parametro :id
-    const id= req.params.id;
-    
-    // obtener indice de proyecto
-    const idpr= projects.find((p)=> p.id == id);
+  if (!project) {
+    res.status(404);
+  }
+  res.send(project);
+});
 
-    // validar si existe indice
-    if(index==-1){
+// elimina elemento
+app.delete("/projects/:id", (req, res) => {
+  // obtener parametro :id
+  const id = req.params.id;
+  // obtener indice de proyecto
+  const index = projects.findIndex((p) => p.id == id);
+  // validar si existe el Indice
+  if (index == -1) {
+    res.status(404);
+    res.send("Not Found!");
+  }
 
-    }
+  // eliminar con splice
+  projects.splice(index, 1);
 
-    // eliminar con splice
-    projects.splice(index, 1);
-
-    // responder "ok!"
-    res.send("ok!");
-})
+  // responder "ok!"
+  res.send("ok!");
+});
 
 // actualizar
-app.put("/projects/:id",(req, res)=>{
-    // extraer id
-    const id= re.params.id;
-    // buscar proyecto
-    const project= projects.find(p=> p.id== id)
-    // validar si se encontro el proyecto
-    if(!project){
-        res.status(404);
-        res.send("Proyecto no encontrado");
-    }
-    // actualizar obj proyecto con el body
-    const body= req.body
-})
+app.put("/projects/:id", (req, res) => {
+  // extraer el id
+  const id = req.params.id;
+  // buscar proyecto
+  const project = projects.find((p) => p.id == id);
+  // validar si se encontro el proyecto
+  if (!project) {
+    res.status(404);
+    res.send("Proyecto no encontrado!");
+  }
 
-app.listen(3000, ()=> console.log("Servidor listo en https://localhost:3000"))
+  // actualizar obj proyecto con el body
+  const body = req.body;
+  project.name = body.name;
+
+  res.send("ok!");
+});
+
+app.listen(3000, () => console.log("Servidor listo en http://localhost:3000"));
